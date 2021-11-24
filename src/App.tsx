33b26ce7b3e590524dd7893d374e028a4106ef8a
dcruz1990 <dcruzbv1990@gmail.com>
2021-11-24 13:11:06 -0500

    Style buttons

--- src/App.tsx
+++ src/App.tsx
@@ -21,13 +21,13 @@ export const App = () => {
         </div>
         <div
           id="button"
-          className="bg-secondary rounded-full w-80 h-80 flex items-center justify-center "
+          className="bg-gray-100 rounded-full w-80 h-80 flex items-center justify-center cursor-pointer  "
         >
           <div
             id="button"
-            className="bg-ffffff text-black text-heading5 uppercase  cursor-pointer hover:border-white border-10 hover:border-8 rounded-full w-60 h-60 absolute flex justify-center items-center "
+            className="bg-ffffff   cursor-pointer hover:border-white border-2 hover:border-8 rounded-full w-60 h-60 absolute flex justify-center items-center hover:shadow-2xl"
           >
-            <p className="bottom-2 left-0">Explore</p>
+            <p className="bottom-2 left-0 text-black text-heading5 uppercase">Explore</p>
           </div>
         </div>
       </div>
